fc75a5d814518e9b34a411bb81b0a65660b3c4db
Gregory Vincic <gregoryvse@gmail.com>
2023-09-24 11:02:31 +0200

    Document

--- map.go
+++ map.go
@@ -7,9 +7,11 @@ import (
 	"strconv"
 )
 
+// Map maps each scanned line to mapping until EOF is reached.
+// Returns ErrSyntax if line is badly formatted.
 func Map(mapping Mapfn, scanner *bufio.Scanner) error {
 	var lineno int
-	// remember last section
+	// last section
 	var section []byte
 
 	for scanner.Scan() {
@@ -17,7 +19,7 @@ func Map(mapping Mapfn, scanner *bufio.Scanner) error {
 		buf := scanner.Bytes()
 		buf = bytes.TrimSpace(buf)
 
-		lbrack, rbrack, equal, hash := indexElements(buf)
+		lbrack, rbrack, equal, semihash := indexElements(buf)
 
 		// grab section, key, value and comment
 		section = grabSection(buf, section, lbrack, rbrack)
@@ -25,7 +27,7 @@ func Map(mapping Mapfn, scanner *bufio.Scanner) error {
 		if err != nil {
 			return fmt.Errorf("%w: line %v: %s", err, lineno, string(buf))
 		}
-		comment := grabComment(buf, hash)
+		comment := grabComment(buf, semihash)
 
 		if !isEmpty(section, key, value, comment) {
 			mapping(
@@ -43,9 +45,11 @@ func Map(mapping Mapfn, scanner *bufio.Scanner) error {
 	return nil
 }
 
-func grabComment(buf []byte, hash int) []byte {
-	if hash == 0 {
-		return buf[hash:]
+// grabComment returns entire buf semihash is 0, nil otherwise,
+// ie. comments are only allowed on separate lines.
+func grabComment(buf []byte, semihash int) []byte {
+	if semihash == 0 {
+		return buf[semihash:]
 	}
 	return nil
 }
@@ -91,15 +95,15 @@ func grabSection(buf, current []byte, lbrack, rbrack int) []byte {
 	return current
 }
 
-func indexElements(buf []byte) (lbrack, rbrack, equal, hash int) {
-	lbrack, rbrack, equal, hash = -1, -1, -1, -1
+func indexElements(buf []byte) (lbrack, rbrack, equal, semihash int) {
+	lbrack, rbrack, equal, semihash = -1, -1, -1, -1
 	for i, b := range buf {
 		setIndex(i, &lbrack, b, '[')
 		setIndex(i, &rbrack, b, ']')
 		setIndex(i, &equal, b, '=')
 
 		if b == '#' || b == ';' {
-			hash = i
+			semihash = i
 			break
 		}
 	}
@@ -124,4 +128,6 @@ func isQuoted(value []byte) bool {
 	return len(value) > 0 && bytes.ContainsAny(value[:1], "\"'`")
 }
 
+// Mapfn is called for each non empty line. section is always the
+// current section. At least one of the arguments is not empty.
 type Mapfn func(section, key, value, comment string) error
