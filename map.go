53bb5818f16fdab3b7743b313fcd9d22552f1577
Gregory Vincic <gregoryvse@gmail.com>
2023-09-24 11:14:23 +0200

    Refactor

--- map.go
+++ map.go
@@ -55,22 +55,23 @@ func grabComment(buf []byte, semihash int) []byte {
 }
 
 func grabKeyValue(buf []byte, equal int) (key, value []byte, err error) {
-	if equal >= 0 {
-		key = bytes.TrimSpace(buf[:equal])
-		if bytes.ContainsAny(key, " ") {
+	if equal == -1 {
+		return
+	}
+	key = bytes.TrimSpace(buf[:equal])
+	if bytes.ContainsAny(key, " ") {
+		return nil, nil, ErrSyntax
+	}
+
+	value = buf[equal+1:]
+	value = bytes.TrimSpace(value)
+	if isQuoted(value) {
+		normalizeQuotes(value)
+		valstr, err := strconv.Unquote(string(value))
+		if err != nil {
 			return nil, nil, ErrSyntax
 		}
-
-		value = buf[equal+1:]
-		value = bytes.TrimSpace(value)
-		if isQuoted(value) {
-			normalizeQuotes(value)
-			valstr, err := strconv.Unquote(string(value))
-			if err != nil {
-				return nil, nil, ErrSyntax
-			}
-			value = []byte(valstr)
-		}
+		value = []byte(valstr)
 	}
 	return
 }
@@ -98,34 +99,40 @@ func grabSection(buf, current []byte, lbrack, rbrack int) []byte {
 func indexElements(buf []byte) (lbrack, rbrack, equal, semihash int) {
 	lbrack, rbrack, equal, semihash = -1, -1, -1, -1
 	for i, b := range buf {
-		setIndex(i, &lbrack, b, '[')
-		setIndex(i, &rbrack, b, ']')
-		setIndex(i, &equal, b, '=')
-
 		if b == '#' || b == ';' {
 			semihash = i
 			break
 		}
+		setIndex(i, &lbrack, b, '[')
+		setIndex(i, &rbrack, b, ']')
+		setIndex(i, &equal, b, '=')
 	}
 	return
 }
 
-func setIndex(i int, to *int, b, is byte) {
-	if b == is {
-		*to = i
+// setIndex updates dst with i if a == b
+func setIndex(i int, dst *int, a, b byte) {
+	if a != b {
+		return
 	}
+	*dst = i
 }
 
+// isEmpty returns true if all arguments are empty
 func isEmpty(section, key, value, comment []byte) bool {
 	return len(section)+len(key)+len(value)+len(comment) == 0
 }
 
+// isSection returns true if lbrack somes before rbrack
 func isSection(lbrack, rbrack int) bool {
 	return rbrack > lbrack && lbrack >= 0 && rbrack >= 0
 }
 
+// isQuoted returns true if the first character of value looks like
+// quote char, value cannot be empty
 func isQuoted(value []byte) bool {
-	return len(value) > 0 && bytes.ContainsAny(value[:1], "\"'`")
+	const quoteChars = "\"'`"
+	return bytes.ContainsAny(value[:1], quoteChars)
 }
 
 // Mapfn is called for each non empty line. section is always the
