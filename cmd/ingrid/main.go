7a85f1af961dc7a1e1bbeb12ba8ce0b2b1b96426
Gregory Vincic <gregoryvse@gmail.com>
2023-09-25 05:41:18 +0200

    Add cmd/ingrid

---
+++ cmd/ingrid/main.go
@@ -0,0 +1,59 @@
+// Command ingrid parses .ini files
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/gregoryv/ingrid"
+)
+
+func main() {
+	next := parseFiles(os.Args[1:])
+	var err error
+	for next != nil {
+		next, err = next()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
+
+func parseFiles(files []string) parseFn {
+	return func() (parseFn, error) {
+		if len(files) == 0 {
+			return nil, nil
+		}
+
+		fh, err := os.Open(files[0])
+		if err != nil {
+			return nil, err
+		}
+		defer fh.Close()
+
+		err = ingrid.Map(
+			printKeyValue, bufio.NewScanner(fh),
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		return parseFiles(files[1:]), nil
+	}
+}
+
+func printKeyValue(section, key, value, comment string) error {
+	if key == "" {
+		return nil
+	}
+	prefix := ""
+	if section != "" {
+		prefix = section + "."
+	}
+	fmt.Printf("%s%s = %s\n", prefix, key, value)
+	return nil
+}
+
+type parseFn func() (parseFn, error)
