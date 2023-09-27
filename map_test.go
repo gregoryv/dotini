826ef092a3c58042f9bb637246dfeb7024732e94
Gregory Vincic <gregoryvse@gmail.com>
2023-09-27 06:15:12 +0200

    Improve example

--- map_test.go
+++ map_test.go
@@ -11,8 +11,8 @@ import (
 func Example() {
 	input := `# generic things
 debug = false
-# used set for all servers
-defaultBind = localhost:80
+# default for servers
+bind= localhost:80
 
 [example]
 text = "escaped \""
@@ -23,6 +23,7 @@ more = 'single "quoted" string'
 hostname=github.com
 bind=localhost:443
 
+# invalid lines
 color
 my name = john
 [trouble
@@ -44,14 +45,14 @@ text='...
 	ingrid.Map(mapping, bufio.NewScanner(strings.NewReader(input)))
 	// output:
 	// debug = false
-	// defaultBind = localhost:80
+	// bind = localhost:80
 	// example.text = escaped "
 	// example.hostname = example.com
 	// example.more = single "quoted" string
 	// github.hostname = github.com
 	// github.bind = localhost:443
-	// input line:15 color missing equal sign: syntax error
-	// input line:16 my name = john space not allowed in key: syntax error
-	// input line:17 [trouble missing right bracket: syntax error
-	// input line:18 text='... missing end quote: syntax error
+	// input line:16 color missing equal sign: syntax error
+	// input line:17 my name = john space not allowed in key: syntax error
+	// input line:18 [trouble missing right bracket: syntax error
+	// input line:19 text='... missing end quote: syntax error
 }
